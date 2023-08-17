a4ea5564a6e3b92d5504f5286911d23cd5540c96
dosboy <dosboy@sina.com>
2023-08-17 20:05:37 +0800

    del some files

--- books/第11章 PyGame大显身手设计情节/11.3.3 图片放大.py
+++
@@ -1,9 +0,0 @@
-import pygame, os, sys,time
-from pygame.locals import *
-from snakeclass import *
-
-
-
-pygame.init()
-screen = pygame.display.set_mode((900,600))
-show_end(screen)
--- books/第9章 PyGame初试牛刀/9.2.2 路径与__FILE__.py
+++
@@ -1,3 +0,0 @@
-import pygame
-print('本文件路径：',__file__)
-print('pygame路径：',pygame.__file__)
